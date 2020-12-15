8ff9b0bdf0c5dd78d7d501f53a4ea0f4056cc1a6
ravindra singh <ravindrarajpoot9628172@gmail.com>
2020-12-15 18:18:53 +0530

    JS Assignment

---
+++ qutation-app/data/index.js
@@ -0,0 +1,24 @@
+let btn = document.querySelector('.btn');
+let heading = document.querySelector('.heading');
+let articleAuthor = document.querySelector('.text')
+let qutoes = document.querySelector('.qutoes');
+let img = document.querySelector('.img');
+let bodyBackGround = document.querySelector('body');
+
+btn.addEventListener("click" , () => {
+    let dataValue = quotes[Math.floor(Math.random() * quotes.length)]
+    let backGroundImg = imgs[Math.floor(Math.random() * imgs.length)]
+    console.log(dataValue);
+    bodyBackGround.style.background = ` linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.5)), url(${backGroundImg.src})`
+    let author = dataValue.quoteAuthor;
+    let qutoesValue = dataValue.quoteText;
+    qutoes.innerText = `${qutoesValue}`
+    articleAuthor.innerText = `${author}`
+})
+setInterval(() => {
+    let backGround = imgs[Math.floor(Math.random() * imgs.length)]
+    bodyBackGround.style.background = ` linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.5)), url(${backGround.src})`
+}, 2000);
+
+
+
