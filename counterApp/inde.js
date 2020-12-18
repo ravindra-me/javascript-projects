b7c4d41353a281736f67c3ca4d2ff8eb02a96bfb
ravindra singh <ravindrarajpoot9628172@gmail.com>
2020-12-18 17:50:06 +0530

    counter app

---
+++ counterApp/inde.js
@@ -0,0 +1,37 @@
+
+function main(){
+   
+    let increment =  document.querySelector('.btn-increment');
+    let decrement = document.querySelector('.btn-decrement');
+    let reset = document.querySelector('.btn-reset');
+    let number = document.querySelector('.secondary-heading');
+
+    let count = 0;
+
+
+    function increageNumber(){
+        count+=1;
+        return number.innerText = count;
+    }
+
+    function decreageNumber(){
+        count-=1;
+        return number.innerText = count;
+    }
+
+    function resetNumber(){
+        count-=1;
+        return number.innerText = 0;
+    }
+
+
+    increment.addEventListener("click", increageNumber);
+    decrement.addEventListener("click", decreageNumber);
+    reset.addEventListener("click", resetNumber);
+
+
+    number.innerText = count;
+
+}
+
+main();
